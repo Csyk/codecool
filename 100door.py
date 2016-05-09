ea65fa0369b76bc1f7bb00ca1fb9de002a886011
Csyk <balazs.paulcsik@gmail.com>
2016-05-09 11:14:10 +0200

    Add files via upload

---
+++ 100door.py
@@ -0,0 +1,15 @@
+ajto = [0 for i in range (100)]
+j = 1
+while j <= 100:
+    i = 0
+    while i < 100:
+        if ajto [i] == 0:
+            ajto [i] = 1
+        else:
+            ajto [i] = 0
+        i = i + j
+    j = j + 1
+
+for i in range (0, 100):
+    if ajto[i] == 1:
+        print(i)
