5669c5745eb35a5025ea1da8d6490b4bc2b260db
Csyk <balazs.paulcsik@gmail.com>
2016-05-09 11:36:28 +0200

    Add files via upload

--- 100door.py
+++ 100door.py
@@ -1,7 +1,7 @@
 ajto = [0 for i in range (100)]
 j = 1
 while j <= 100:
-    i = 0
+    i = j - 1
     while i < 100:
         if ajto [i] == 0:
             ajto [i] = 1
@@ -12,4 +12,4 @@ while j <= 100:
 
 for i in range (0, 100):
     if ajto[i] == 1:
-        print(i)
+        print(i + 1)
